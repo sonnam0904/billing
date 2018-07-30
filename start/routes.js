52208aa06b0a30183d8abe6fcf380e669a4c9816
DESKTOP-D3QUBUU\daochien <chiendv@vnedutech.vn>
2018-07-30 17:38:03 +0700

    no message

--- start/routes.js
+++ start/routes.js
@@ -22,7 +22,9 @@ Route.group(() => {
         Route.get('login', 'UserController.login').as('user.user.login')
         Route.post('sign', 'UserController.sign').as('user.user.sign');
         Route.post('register', 'UserController.register').as('user.user.register');
-        Route.get('/:id', 'UserController.detail').as('user.user.detail')
+        Route.get('logout', 'UserController.logout').as('user.user.logout')
+        //Route.get('/:id', 'UserController.detail').as('user.user.detail').middleware('auth');
+        
     })
     .prefix('user')
     // .middleware(['auth'])
